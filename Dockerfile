096ad4550937670225926bfb002ff158552a2965
Maciej <maciej.wieczorek@fastwhitecat.com>
2017-02-23 20:28:29 +0100

    script to compress images

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,13 @@ RUN chmod +x /etc/sv/nfs/finish
 ADD nfs_setup.sh /usr/local/bin/nfs_setup
 RUN chmod +x /usr/local/bin/nfs_setup
 
+###############################################################################
+#                                    COMPRESS IMAGES CONF
+###############################################################################
+
+ADD ./compress.sh /usr/local/bin/compress-images.sh
+RUN chmod +x /usr/local/bin/compress-images.sh
+
 VOLUME /exports
 
 EXPOSE 111/udp 2049/tcp
---
+++ compress-images.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --dir=*)
+      dir="${1#*=}"
+      ;;
+    --ext=*)
+      files="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+files="$(find -L "$dir" -type f -name "$ext")"
+countFiles=$(echo -n "$files" | wc -l)
+echo "Count: $countFiles"
+echo "$files" | while read file; do
+ jpegoptim $file
+done
+
+
